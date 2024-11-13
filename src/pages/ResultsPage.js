e5417db9cc37a4a958a36b301f0d95041efdd614
GumSurf <gabriel.christe@gmail.com>
2024-11-13 16:31:50 +0100

    Add: Results page layout and search display

---
+++ src/pages/ResultsPage.js
@@ -0,0 +1,12 @@
+import SearchResult from '../components/SearchResults';
+
+const ResultsPage = () => {
+
+    return (
+        <div>
+            <SearchResult />
+        </div>
+    );
+};
+
+export default ResultsPage;
