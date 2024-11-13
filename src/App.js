3380ed0c2a0927956982d1c2f7ea259c42029c0b
GumSurf <gabriel.christe@gmail.com>
2024-11-13 16:24:23 +0100

    Add: routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/index.css';
+import CocktailDetail from './pages/CocktailDetail';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import ResultsPage from './pages/ResultsPage';
+import CategoriesPage from './pages/CategoriesPage';
+import CategoryCocktails from './components/CategoryCocktails';
+import IngredientDetail from './pages/IngredientDetail';
+import CocktailRandom from './pages/CocktailRandom';
+import HeaderTest from './components/HeaderTest';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Header />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search-result" element={<ResultsPage />} />
+            <Route path="/cocktail/:id" element={<CocktailDetail />} />
+            <Route path="/ingredient/:ingredientName" element={<IngredientDetail />} />
+            <Route path="/cocktail/:cocktailOrIngredient/:id" element={<CocktailDetail />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="/categories/:categoryId" element={<CategoryCocktails />} />
+            <Route path="/random" element={<CocktailRandom />} />
+            <Route path='/header_test' element={<HeaderTest />} />
+        </Routes>
+    </Router>
   );
 }
 
