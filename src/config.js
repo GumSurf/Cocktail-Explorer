b7b3d5b657206db05cc79657b9ed8829c92f2fc6
GumSurf <gabriel.christe@gmail.com>
2024-11-13 16:23:50 +0100

    Add: file config size image cocktailDB

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
+const IMAGE_URL = 'https://www.thecocktaildb.com/images/ingredients/';
+const SMALL_IMAGE = '-Small.png';
+const MEDIUM_IMAGE = '-Medium.png';
+const LARGE_IMAGE = '.png';
+
+export { API_URL, IMAGE_URL, SMALL_IMAGE, MEDIUM_IMAGE, LARGE_IMAGE };
